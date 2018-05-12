843696768e5e3cacb7186b03ea906da178d10386
Ian Woodbury <sparkworx@gmail.com>
2018-05-11 21:55:29 -0700

    [yubico-yubihsm2-sdk] update cask to version 1.0.3

--- Casks/yubico-yubihsm2-sdk.rb
+++ Casks/yubico-yubihsm2-sdk.rb
@@ -1,6 +1,6 @@
 cask 'yubico-yubihsm2-sdk' do
-  version '1.0.2'
-  sha256 'b71492fb97850ce7891424e91e63d0133d61641aead211c8f74aadbd42db08e2'
+  version '1.0.3'
+  sha256 '3424c14093d880eda6c445f265136bafbaaa526988036537b577e40e9c24d09f'
 
   url "https://developers.yubico.com/YubiHSM2/Releases/yubihsm2-sdk-#{version}-darwin-amd64.tar.gz"
   name 'Yubico YubiHSM 2 SDK'
