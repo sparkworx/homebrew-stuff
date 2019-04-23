7ae85318fa101b6a71b81095104ffb61bd91c782
Ian Woodbury <sparkworx@gmail.com>
2019-04-23 09:12:05 -0700

    Updated (belatedly) for SDK v2.0.0

--- Casks/yubico-yubihsm2-sdk.rb
+++ Casks/yubico-yubihsm2-sdk.rb
@@ -1,14 +1,14 @@
 cask 'yubico-yubihsm2-sdk' do
-  version '1.0.4'
-  sha256 '8526e02dd3ecbd6ec7e536dff084d5bb3870842db567f57600eb409739eada8f'
+  version '2.0.0'
+  sha256 '1f7adf2f15cd041b21ee878fd90957e562f77636af1af451c1b611e2d32073ce'
 
   url "https://developers.yubico.com/YubiHSM2/Releases/yubihsm2-sdk-#{version}-darwin-amd64.tar.gz"
   name 'Yubico YubiHSM 2 SDK'
   homepage 'https://developers.yubico.com/YubiHSM2/'
 
-  binary 'yubihsm2-sdk/bin/yhwrap'
   binary 'yubihsm2-sdk/bin/yubihsm-connector'
   binary 'yubihsm2-sdk/bin/yubihsm-setup'
   binary 'yubihsm2-sdk/bin/yubihsm-shell'
+  binary 'yubihsm2-sdk/bin/yubihsm-wrap'
 
 end
