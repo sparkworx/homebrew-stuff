7f744ec1e2bf2bf7459238aa0264d9e750d72aad
Ian Woodbury <sparkworx@gmail.com>
2019-04-23 09:57:18 -0700

    Fix versioning scheme for msieve formula

--- Formula/msieve.rb
+++ Formula/msieve.rb
@@ -1,7 +1,9 @@
 class Msieve < Formula
   desc "Factor large integers"
+  version "1.52"
+  version_scheme 1
   homepage "https://sourceforge.net/projects/msieve/"
-  url "https://downloads.sourceforge.net/project/msieve/msieve/Msieve%20v1.52/msieve152.tar.gz"
+  url "https://downloads.sourceforge.net/project/msieve/msieve/Msieve%20v#{version}/msieve#{version.to_s.delete('.')}.tar.gz"
   sha256 "8a895544d316b8befe0e93a1fd7a1d508e8aa0f734035e2632aa4d928a2ef20b"
 
   depends_on "gcc"
