42af1db39329e3342b9108c2961545d69aaace63
Ian Woodbury <sparkworx@gmail.com>
2019-04-23 10:00:43 -0700

    Updated msieve formula to v1.53

--- Formula/msieve.rb
+++ Formula/msieve.rb
@@ -1,10 +1,10 @@
 class Msieve < Formula
   desc "Factor large integers"
-  version "1.52"
+  version "1.53"
   version_scheme 1
   homepage "https://sourceforge.net/projects/msieve/"
-  url "https://downloads.sourceforge.net/project/msieve/msieve/Msieve%20v#{version}/msieve#{version.to_s.delete('.')}.tar.gz"
-  sha256 "8a895544d316b8befe0e93a1fd7a1d508e8aa0f734035e2632aa4d928a2ef20b"
+  url "https://downloads.sourceforge.net/project/msieve/msieve/Msieve%20v#{version}/msieve#{version.to_s.delete('.')}_src.tar.gz"
+  sha256 "c5fcbaaff266a43aa8bca55239d5b087d3e3f138d1a95d75b776c04ce4d93bb4"
 
   depends_on "gcc"
 
