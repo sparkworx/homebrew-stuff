ac619d85f7192ff587ba5163bb0a161735584794
Ian Woodbury <sparkworx@gmail.com>
2022-02-16 22:34:01 -0700

    Update yubico-yubihsm2-sdk from 2021-04 to 2021-08
    - switch from amd64 pkg to universal
    - update livecheck to look for universal package updates

--- Casks/yubico-yubihsm2-sdk.rb
+++ Casks/yubico-yubihsm2-sdk.rb
@@ -1,8 +1,8 @@
 cask 'yubico-yubihsm2-sdk' do
-  version '2021-04'
-  sha256 '081dedd97cbdda5cde6cff2898d74cd78ba9a3f1af9d3225e92ade9669800c6d'
+  version '2021-08'
+  sha256 '73e5d060042b625c150a9c1bf64863db46a3dc7976465cb2222b16dc0e9bbd8f'
 
-  url "https://developers.yubico.com/YubiHSM2/Releases/yubihsm2-sdk-#{version}-darwin-amd64.pkg"
+  url "https://developers.yubico.com/YubiHSM2/Releases/yubihsm2-sdk-#{version}-darwin-universal.pkg"
   name 'Yubico YubiHSM 2 SDK'
   desc 'SDK and operational software for Yubico YubiHSM 2'
   homepage 'https://developers.yubico.com/YubiHSM2/'
@@ -10,10 +10,10 @@ cask 'yubico-yubihsm2-sdk' do
   livecheck do
     url "https://developers.yubico.com/YubiHSM2/Releases/"
     strategy :page_match
-    regex(/yubihsm2-sdk-(\d{4}-\d{2}[a-z]?)-darwin-amd64/i)
+    regex(/yubihsm2-sdk-(\d{4}-\d{2}[a-z]?)-darwin-universal/i)
   end
 
-  pkg "yubihsm2-sdk-#{version}-darwin-amd64.pkg"
+  pkg "yubihsm2-sdk-#{version}-darwin-universal.pkg"
 
   uninstall pkgutil: "com.yubico.yubihsm2-sdk"
 
