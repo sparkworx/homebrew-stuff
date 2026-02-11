0770846d6f29fb8c9c7a27dfedb373f87ccdd450
Ian Woodbury <sparkworx@gmail.com>
2026-02-11 13:34:36 -0700

    yubihsm-setup: update 2.3.4 formula.

--- Formula/y/yubihsm-setup.rb
+++ Formula/y/yubihsm-setup.rb
@@ -1,10 +1,11 @@
 class YubihsmSetup < Formula
   desc "Yubico YubiHSM Setup Tool"
-  homepage "https://github.com/Yubico/yubihsm-setup"
-  url "https://github.com/Yubico/yubihsm-setup/archive/refs/tags/2.3.3.tar.gz"
-  sha256 "b92404309324a2b9fe995a5ac2ccf669fb3c46d5fe239f11627f1f42476db031"
+  homepage "https://developers.yubico.com/yubihsm-setup/"
+  url "https://github.com/Yubico/yubihsm-setup/archive/refs/tags/2.3.4.tar.gz"
+  sha256 "0245b24031aa6ab1ffcf094adfcfe9c3ed19b0c10b341346b4b74c6ec4620292"
   license "Apache-2.0"
 
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "sparkworx/stuff/yubihsm-shell"
 
@@ -13,31 +14,21 @@ class YubihsmSetup < Formula
     sha256 "51fbc09b1fd2232e99e73971d5503b66608d757f55fe5e12db1597cdeafd787c"
   end
 
-  patch :DATA
-
   def install
-    resource("yubihsmrs").stage do
-      (buildpath/"yubihsmrs").install Dir["*"]
-    end
-
+    resource("yubihsmrs").stage(buildpath/"yubihsmrs")
+    inreplace "Cargo.toml", 'path = "../yubihsmrs/"', 'path = "yubihsmrs/"'
     system "cargo", "install", *std_cargo_args
   end
 
+  def caveats
+    <<~EOS
+      This formula may conflict with the yubihsm2-sdk cask, which installs
+      the same components via Yubico's installer package. If you have the
+      cask installed, run: brew uninstall --cask yubihsm2-sdk
+    EOS
+  end
+
   test do
-    assert_match "yubihsm-setup #{version}", shell_output("#{bin}/yubihsm-setup --version")
+    assert_match version.to_s, shell_output("#{bin}/yubihsm-setup --version")
   end
 end
-__END__
-diff --git a/Cargo.toml b/Cargo.toml
-index 3b7ec34..0bdc5ac 100644
---- a/Cargo.toml
-+++ b/Cargo.toml
-@@ -30,7 +30,7 @@ rusty_secrets = "0.0.2"
- scan_dir = "0.3.3"
-
- [dependencies.yubihsmrs]
--path = "../yubihsmrs/"
-+path = "./yubihsmrs/"
- version = "2.1.4"
-
- [package.metadata.deb]
