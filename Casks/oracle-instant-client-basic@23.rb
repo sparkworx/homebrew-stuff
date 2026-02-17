addba76884ef752a7f677101182fd26cbc54cc77
Ian Woodbury <sparkworx@gmail.com>
2026-02-17 02:10:03 -0700

    fixup! oracle-instant-client@19: add 23.3.0.0.0 casks (ARM-only)

--- Casks/oracle-instant-client-basic@23.rb
+++ Casks/oracle-instant-client-basic@23.rb
@@ -52,9 +52,7 @@ cask "oracle-instant-client-basic@23" do
 
   preflight do
     system_command "/bin/mkdir", args: ["-p", ic_dir], sudo: true
-    system_command "/bin/bash",
-                   args: ["-c", "/usr/sbin/chown $(stat -f '%Su:%Sg' '#{HOMEBREW_PREFIX}') '#{ic_dir}'"],
-                   sudo: true
+    system_command "/usr/sbin/chown", args: ["#{Process.uid}:#{Process.gid}", ic_dir], sudo: true
     system_command "/bin/mkdir", args: ["-p", "#{ic_dir}/network/admin"]
   end
 
@@ -85,7 +83,7 @@ cask "oracle-instant-client-basic@23" do
   end
 
   uninstall_postflight do
-    system_command "/usr/bin/find", args: [ic_dir, "-type", "d", "-empty", "-delete"]
+    system_command "/usr/bin/find", args: [ic_dir, "-type", "d", "-empty", "-delete"], sudo: true
   end
 
   zap trash: [ic_dir],
